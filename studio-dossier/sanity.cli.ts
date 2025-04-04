d608a12a29bf389c4e119995bcc32682b6393030
Okechuqu <chukwummnl@gmail.com>
2025-04-04 12:30:40 +0100

    add studioHost

--- studio-dossier/sanity.cli.ts
+++ studio-dossier/sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'kx25p8c1',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'dossier',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
