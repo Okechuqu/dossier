d87c2ed7918fbdc3c0e9965da0a657de4d221f2b
Okechuqu <chukwummnl@gmail.com>
2025-04-05 10:23:18 +0100

    timeline adjustment

--- next-dossier/app/components/ui/timeline.tsx
+++ next-dossier/app/components/ui/timeline.tsx
@@ -40,7 +40,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full md:bg-green-500 border border-green-300 p-2" />
+                <div className="h-4 w-4 rounded-full bg-green-500 border border-green-300 p-2" />
               </div>
               <h3 className="hidden md:block text-green-500 text-base md:pl-20 md:text-3xl font-bold">
                 {" "}
